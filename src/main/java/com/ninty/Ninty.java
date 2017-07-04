768c228cafb34036e2dc848625085b467fbf1a0e
ninty <c19354837@hotmail.com>
2017-07-04 22:30:00 +0800

    init code

---
+++ src/main/java/com/ninty/Ninty.java
@@ -0,0 +1,11 @@
+package com.ninty;
+
+/**
+ * Created by ninty on 2017/7/4.
+ */
+public class Ninty {
+
+    public static void main(String[] args) {
+        System.out.println("It's ninty");
+    }
+}
