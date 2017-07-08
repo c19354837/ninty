47159d98a32b0683c59346c11c38acd39dd5255f
ninty <c19354837@hotmail.com>
2017-07-09 00:32:51 +0800

    check string

---
+++ src/main/java/com/ninty/utils/VMUtils.java
@@ -0,0 +1,11 @@
+package com.ninty.utils;
+
+/**
+ * Created by ninty on 2017/7/8.
+ */
+public class VMUtils {
+
+    public static boolean isEmpty(String str){
+        return str == null || str.trim().equals("");
+    }
+}
