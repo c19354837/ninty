f1d925317125553850fc97b16689dcf3e0bf624e
ninty <c19354837@hotmail.com>
2017-07-17 23:35:48 +0800

    constant&load cmd

--- src/main/java/com/ninty/cmd/CmdConstants.java
+++ src/main/java/com/ninty/cmd/CmdConstants.java
@@ -1,7 +1,149 @@
 package com.ninty.cmd;
 
+import com.ninty.cmd.base.ICmdBase;
+import com.ninty.cmd.base.NoOperandCmd;
+import com.ninty.runtime.NiFrame;
+
+import java.nio.ByteBuffer;
+
 /**
  * Created by ninty on 2017/7/16.
  */
 public class CmdConstants {
+
+    static class NOP extends NoOperandCmd{
+    }
+
+    static class ACONST_NULL extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushRef(null);
+        }
+    }
+
+    static class DCONST_0 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushDouble(0.0);
+        }
+    }
+
+    static class DCONST_1 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushDouble(1.0);
+        }
+    }
+
+    static class FCONST_0 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushFloat(0.0f);
+        }
+    }
+
+    static class FCONST_1 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushFloat(1.0f);
+        }
+    }
+
+    static class FCONST_2 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushFloat(2.0f);
+        }
+    }
+
+    static class ICONST_M1 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(-1);
+        }
+    }
+
+    static class ICONST_0 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(0);
+        }
+    }
+
+    static class ICONST_1 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(1);
+        }
+    }
+
+    static class ICONST_2 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(2);
+        }
+    }
+
+    static class ICONST_3 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(3);
+        }
+    }
+
+    static class ICONST_4 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(4);
+        }
+    }
+
+    static class ICONST_5 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(5);
+        }
+    }
+
+    static class LCONST_0 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(0);
+        }
+    }
+
+    static class LCONST_1 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(1);
+        }
+    }
+
+    static class BIPUSH implements ICmdBase{
+        int val;
+
+        @Override
+        public void init(ByteBuffer bb) {
+            val = bb.get();
+        }
+
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(val);
+        }
+    }
+
+    static class SIPUSH implements ICmdBase{
+        int val;
+
+        @Override
+        public void init(ByteBuffer bb) {
+            val = bb.getChar();
+        }
+
+        @Override
+        public void exec(NiFrame frame) {
+            frame.getOperandStack().pushInt(val);
+        }
+    }
 }
--- src/main/java/com/ninty/cmd/CmdLoads.java
+++ src/main/java/com/ninty/cmd/CmdLoads.java
@@ -1,7 +1,51 @@
 package com.ninty.cmd;
 
+import com.ninty.cmd.base.Index8Cmd;
+import com.ninty.cmd.base.NoOperandCmd;
+import com.ninty.runtime.NiFrame;
+
 /**
  * Created by ninty on 2017/7/16.
  */
 public class CmdLoads {
+
+    static void iLoad(NiFrame frame, int index){
+        int val = frame.getLocalVars().getInt(index);
+        frame.getOperandStack().pushInt(val);
+    }
+
+    static class ILOAD extends Index8Cmd{
+        @Override
+        public void exec(NiFrame frame) {
+            iLoad(frame, index);
+        }
+    }
+
+    static class ILOAD_0 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            iLoad(frame, 0);
+        }
+    }
+
+    static class ILOAD_1 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            iLoad(frame, 1);
+        }
+    }
+
+    static class ILOAD_2 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            iLoad(frame, 2);
+        }
+    }
+
+    static class ILOAD_3 extends NoOperandCmd{
+        @Override
+        public void exec(NiFrame frame) {
+            iLoad(frame, 3);
+        }
+    }
 }
