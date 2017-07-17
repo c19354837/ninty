31cafa8a4946702ebbd5541b1f0cf4b772d33c7e
ninty <c19354837@hotmail.com>
2017-07-17 23:33:43 +0800

    convert byte to unsign int

--- src/main/java/com/ninty/utils/VMUtils.java
+++ src/main/java/com/ninty/utils/VMUtils.java
@@ -8,4 +8,8 @@ public class VMUtils {
     public static boolean isEmpty(String str){
         return str == null || str.trim().equals("");
     }
+
+    public static int toUInt(byte val){
+        return val&0xff;
+    }
 }
