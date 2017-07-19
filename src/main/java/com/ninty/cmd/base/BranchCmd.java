c20fe6c785d9c74d014175da11bb31c38df51f2e
ninty <c19354837@hotmail.com>
2017-07-19 23:32:05 +0800

    offset for branch

--- src/main/java/com/ninty/cmd/base/BranchCmd.java
+++ src/main/java/com/ninty/cmd/base/BranchCmd.java
@@ -1,7 +1,16 @@
 package com.ninty.cmd.base;
 
+import java.nio.ByteBuffer;
+
 /**
  * Created by ninty on 2017/7/16.
  */
 public abstract class BranchCmd implements ICmdBase {
+
+    protected int offset;
+
+    @Override
+    public void init(ByteBuffer bb) {
+        offset = bb.getShort();
+    }
 }
