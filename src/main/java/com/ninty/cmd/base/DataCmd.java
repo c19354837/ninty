4921e99e8551dcd2168014ac8ac31bfe2c80a84a
ninty <c19354837@hotmail.com>
2017-07-20 23:19:40 +0800

    for jumping

---
+++ src/main/java/com/ninty/cmd/base/DataCmd.java
@@ -0,0 +1,19 @@
+package com.ninty.cmd.base;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by ninty on 2017/7/20.
+ */
+public abstract class DataCmd implements ICmdBase {
+    protected ByteBuffer bb;
+
+    @Override
+    public void init(ByteBuffer bb) {
+        this.bb = bb;
+    }
+
+    public void jumpTo(int offset){
+        bb.position(bb.position()+offset);
+    }
+}
