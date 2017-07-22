5c438fde614d5441c1bb6bda40cd4705f56e65c7
ninty <c19354837@hotmail.com>
2017-07-22 16:01:55 +0800

    branch to the right offset

--- src/main/java/com/ninty/cmd/base/DataCmd.java
+++ src/main/java/com/ninty/cmd/base/DataCmd.java
@@ -7,13 +7,15 @@ import java.nio.ByteBuffer;
  */
 public abstract class DataCmd implements ICmdBase {
     protected ByteBuffer bb;
+    private int index;
 
     @Override
     public void init(ByteBuffer bb) {
         this.bb = bb;
+        index = bb.position() - 1;
     }
 
     public void jumpTo(int offset) {
-        bb.position(bb.position() + offset);
+        bb.position(index + offset);
     }
 }
