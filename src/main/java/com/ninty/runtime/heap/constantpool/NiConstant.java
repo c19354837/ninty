619a837b17a5439de2e84bc74decc941ef1d7320
ninty <c19354837@hotmail.com>
2017-07-28 23:54:20 +0800

    cp

--- src/main/java/com/ninty/runtime/heap/constantpool/NiConstant.java
+++ src/main/java/com/ninty/runtime/heap/constantpool/NiConstant.java
@@ -7,6 +7,7 @@ import com.sun.org.apache.bcel.internal.classfile.ClassFormatException;
  * Created by ninty on 2017/7/23.
  */
 public class NiConstant {
+    NiConstantPool cp;
 
     public static NiConstant generate(ConstantInfo cp) {
         NiConstant nicp;
