9a3d3cd1f5d281f7be2166ba017d939e080e0e9e
ninty <c19354837@hotmail.com>
2017-07-29 23:49:48 +0800

    set&get slot

--- src/main/java/com/ninty/runtime/LocalVars.java
+++ src/main/java/com/ninty/runtime/LocalVars.java
@@ -66,6 +66,14 @@ public class LocalVars {
         slots[index].ref = ref;
     }
 
+    public Slot getSlot(int index){
+        return slots[index];
+    }
+
+    public void setSlot(int index, Slot slot){
+        slots[index] = slot;
+    }
+
     @Override
     public String toString() {
         return "LocalVars{" +
