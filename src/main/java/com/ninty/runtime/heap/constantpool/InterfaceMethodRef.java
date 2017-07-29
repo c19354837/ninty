d317cdeb1cbb1c80bbfc0227ed6d09e7f30f4a0c
ninty <c19354837@hotmail.com>
2017-07-29 23:07:26 +0800

    resolve methodRef&interfaceMethodRef

--- src/main/java/com/ninty/runtime/heap/constantpool/InterfaceMethodRef.java
+++ src/main/java/com/ninty/runtime/heap/constantpool/InterfaceMethodRef.java
@@ -1,17 +1,55 @@
 package com.ninty.runtime.heap.constantpool;
 
 import com.ninty.classfile.constantpool.ConstantInfo;
+import com.ninty.runtime.heap.NiClass;
+import com.ninty.runtime.heap.NiMethod;
 
 /**
  * Created by ninty on 2017/7/24.
  */
 public class InterfaceMethodRef extends MemberRef {
+    private NiMethod method;
+
     public InterfaceMethodRef(ConstantInfo.CPMemeber cp) {
         super(cp);
     }
 
     @Override
     public void resolve() {
-        // TODO
+        if (method == null) {
+            resolveClass();
+            if (!clz.isInterface()) {
+                throw new IncompatibleClassChangeError("expect for interface while it's class: " + clz);
+            }
+            NiMethod method = lookUpMethods(clz, name, desc);
+            if (method == null) {
+                throw new NoSuchMethodError(this.toString());
+            }
+            if (!method.canAccess(cp.clz)) {
+                throw new IllegalAccessError(cp.clz + " cannot access " + method);
+            }
+            this.method = method;
+        }
+    }
+
+    private NiMethod lookUpMethods(NiClass clz, String name, String desc) {
+        for (NiMethod method : clz.getMethods()) {
+            if (method.getName().equals(name) && method.getDesc().equals(desc)) {
+                return method;
+            }
+        }
+
+        for (NiClass c : clz.getInterfaces()) {
+            NiMethod method = lookUpMethods(c, name, desc);
+            if(method != null){
+                return method;
+            }
+        }
+
+        return null;
+    }
+
+    public NiMethod getMethod() {
+        return method;
     }
 }
--- src/main/java/com/ninty/runtime/heap/constantpool/MethodRef.java
+++ src/main/java/com/ninty/runtime/heap/constantpool/MethodRef.java
@@ -1,6 +1,7 @@
 package com.ninty.runtime.heap.constantpool;
 
 import com.ninty.classfile.constantpool.ConstantInfo;
+import com.ninty.runtime.heap.NiClass;
 import com.ninty.runtime.heap.NiMethod;
 
 /**
@@ -15,10 +16,56 @@ public class MethodRef extends MemberRef {
 
     @Override
     public void resolve() {
-        // TODO
+        if(method == null){
+            resolveClass();
+            if(clz.isInterface()){
+                throw new IncompatibleClassChangeError("expect for class while it's interface: " + clz);
+            }
+            NiMethod method = lookUpMethods(clz, name, desc);
+            if(method == null){
+                throw new NoSuchMethodError(this.toString());
+            }
+            if(!method.canAccess(cp.clz)){
+                throw new IllegalAccessError(cp.clz + " cannot access " + method);
+            }
+            this.method = method;
+        }
     }
+
+    private NiMethod lookUpMethods(NiClass clz, String name, String desc) {
+        for (NiMethod method : clz.getMethods()) {
+            if (method.getName().equals(name) && method.getDesc().equals(desc)) {
+                return method;
+            }
+        }
+
+        if (clz.getSuperClass() != null) {
+            NiMethod method = lookUpMethods(clz.getSuperClass(), name, desc);
+            if(method != null){
+                return method;
+            }
+        }
+
+        for (NiClass c : clz.getInterfaces()) {
+            NiMethod method = lookUpMethods(c, name, desc);
+            if(method != null){
+                return method;
+            }
+        }
+        return null;
+    }
+
 
     public NiMethod getMethod() {
         return method;
+    }
+
+    @Override
+    public String toString() {
+        return "MethodRef{" +
+                "className='" + className + '\'' +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
     }
 }
