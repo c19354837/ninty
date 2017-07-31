d0ad13c3062d2fe74f4d89360ec112afc2e3151e
ninty <c19354837@hotmail.com>
2017-07-31 23:16:52 +0800

    support array Object

--- src/main/java/com/ninty/runtime/heap/NiObject.java
+++ src/main/java/com/ninty/runtime/heap/NiObject.java
@@ -8,6 +8,7 @@ import com.ninty.runtime.LocalVars;
 public class NiObject {
     private NiClass clz;
     private LocalVars fields;
+    private Object arrayDatas; // when it's array
 
     public NiObject(NiClass clz, int count) {
         this.clz = clz;
@@ -19,12 +20,71 @@ public class NiObject {
         this.fields = obj.fields;
     }
 
+    public NiObject(NiClass clz, Object arrayDatas) {
+        this.clz = clz;
+        this.arrayDatas = arrayDatas;
+    }
+
     public NiClass getClz() {
         return clz;
     }
 
     public LocalVars getFields() {
         return fields;
+    }
+
+    public byte[] abyte() {
+        return (byte[]) arrayDatas;
+    }
+
+    public short[] ashort() {
+        return (short[]) arrayDatas;
+    }
+
+    public char[] achar() {
+        return (char[]) arrayDatas;
+    }
+
+    public int[] aint() {
+        return (int[]) arrayDatas;
+    }
+
+    public long[] along() {
+        return (long[]) arrayDatas;
+    }
+
+    public float[] afloat() {
+        return (float[]) arrayDatas;
+    }
+
+    public double[] adouble() {
+        return (double[]) arrayDatas;
+    }
+
+    public NiObject[] aobject() {
+        return (NiObject[]) arrayDatas;
+    }
+
+    public int arrayLength() {
+        Class clz = arrayDatas.getClass();
+        if (clz == byte[].class) {
+            return abyte().length;
+        } else if (clz == short[].class) {
+            return ashort().length;
+        } else if (clz == char[].class) {
+            return achar().length;
+        } else if (clz == int[].class) {
+            return aint().length;
+        } else if (clz == long[].class) {
+            return along().length;
+        } else if (clz == float[].class) {
+            return afloat().length;
+        } else if (clz == double[].class) {
+            return adouble().length;
+        } else if (clz == NiObject[].class) {
+            return aobject().length;
+        }
+        throw new IllegalAccessError("Only array can access length");
     }
 
     public boolean isInstanceOf(NiClass clz) {
