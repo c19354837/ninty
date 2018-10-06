aceaf670f1bc46cb2ba8b8ab7ba97c1d602a9096
ninty <c19354837@hotmail.com>
2018-10-06 13:13:32 +0800

    get field type

--- src/main/java/com/ninty/runtime/heap/NiField.java
+++ src/main/java/com/ninty/runtime/heap/NiField.java
@@ -31,4 +31,33 @@ public class NiField extends ClassMember {
     public int getSlotId() {
         return slotId;
     }
+
+    public NiClass getType() {
+        switch (desc.charAt(0)) {
+            case 'B':
+                return clz.loader.loadClass("byte");
+            case 'C':
+                return clz.loader.loadClass("char");
+            case 'D':
+                return clz.loader.loadClass("double");
+            case 'F':
+                return clz.loader.loadClass("float");
+            case 'I':
+                return clz.loader.loadClass("int");
+            case 'J':
+                return clz.loader.loadClass("long");
+            case 'S':
+                return clz.loader.loadClass("short");
+            case 'Z':
+                return clz.loader.loadClass("boolean");
+            case 'V':
+                return clz.loader.loadClass("void");
+            case '[':
+                return clz.getLoader().loadClass(desc);
+            case 'L':
+                return clz.getLoader().loadClass(desc.substring(1, desc.length() - 1));
+            default:
+                throw new UnsupportedOperationException("unkonw type:" + desc);
+        }
+    }
 }
