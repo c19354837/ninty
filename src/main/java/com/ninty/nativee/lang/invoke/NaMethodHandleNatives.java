2efe18f7b4656defbfd3f6012d370fabcdf181a6
ninty <c19354837@hotmail.com>
2018-10-10 19:10:07 +0800

    add native method: java.lang.invoke.MethodHandleNatives.resolve(Ljava/lang/invoke/MemberName;Ljava/lang/Class;)Ljava/lang/invoke/MemberName;
    todo

--- src/main/java/com/ninty/nativee/lang/invoke/NaMethodHandleNatives.java
+++ src/main/java/com/ninty/nativee/lang/invoke/NaMethodHandleNatives.java
@@ -3,6 +3,7 @@ package com.ninty.nativee.lang.invoke;
 import com.ninty.nativee.INativeMethod;
 import com.ninty.nativee.NaMethodManager;
 import com.ninty.runtime.NiFrame;
+import com.ninty.runtime.heap.NiObject;
 
 public class NaMethodHandleNatives {
     private final static String className = "java/lang/invoke/MethodHandleNatives";
@@ -10,6 +11,8 @@ public class NaMethodHandleNatives {
     public static void init() {
         NaMethodManager.register(className, "getConstant", "(I)I", new
                 getConstant());
+        NaMethodManager.register(className, "resolve", "(Ljava/lang/invoke/MemberName;Ljava/lang/Class;)Ljava/lang/invoke/MemberName;", new
+                resolve());
     }
 
     public static class getConstant implements INativeMethod {
@@ -18,6 +21,15 @@ public class NaMethodHandleNatives {
         public void invoke(NiFrame frame) {
             int which = frame.getLocalVars().getInt(0);
             frame.getOperandStack().pushInt(which == 4 ? 1 : 0);
+        }
+    }
+
+    public static class resolve implements INativeMethod {
+
+        @Override
+        public void invoke(NiFrame frame) {
+            NiObject memberName = frame.getLocalVars().getRef(0);
+            frame.getOperandStack().pushRef(memberName);
         }
     }
 }
