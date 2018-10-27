c3523531010cd5cdd449daed5658867ae54d7dcf
ninty <c19354837@hotmail.com>
2018-10-27 17:33:36 +0800

    add native method: java.io.FileOutputStream.writeBytes([BIIZ)V

--- src/main/java/com/ninty/nativee/io/NaFileOutputStream.java
+++ src/main/java/com/ninty/nativee/io/NaFileOutputStream.java
@@ -2,7 +2,9 @@ package com.ninty.nativee.io;
 
 import com.ninty.nativee.INativeMethod;
 import com.ninty.nativee.NaMethodManager;
+import com.ninty.runtime.LocalVars;
 import com.ninty.runtime.NiFrame;
+import com.ninty.runtime.heap.NiObject;
 
 /**
  * Created by ninty on 2017/10/21.
@@ -13,11 +15,24 @@ public class NaFileOutputStream {
     public static void init() {
         NaMethodManager.register(className, "initIDs", "()V",
                 new initIDs());
+        NaMethodManager.register(className, "writeBytes", "([BIIZ)V",
+                new writeBytes());
     }
 
     public static class initIDs implements INativeMethod {
         @Override
         public void invoke(NiFrame frame) {
+        }
+    }
+
+    public static class writeBytes implements INativeMethod {
+        @Override
+        public void invoke(NiFrame frame) {
+            LocalVars localVars = frame.getLocalVars();
+            NiObject datas = localVars.getRef(1);
+            int offset = localVars.getInt(2);
+            int length = localVars.getInt(3);
+            System.out.write(datas.abyte(), offset, length);
         }
     }
 }
